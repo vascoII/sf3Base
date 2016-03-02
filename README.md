752de04e6474da830364846b663b43fc05f7b85c
fabrice <fabrice@fabrice-PC>
2016-03-02 20:52:06 +0100

    1er Bundle

---
+++ README.md
@@ -0,0 +1,4 @@
+sf3Base
+=======
+
+A Symfony project created on March 2, 2016, 7:49 pm.
---
+++ src/Vasco/CrudBundle/VascoCrudBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Vasco\CrudBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VascoCrudBundle extends Bundle
+{
+}
