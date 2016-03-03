357fa67aed0794abb2ba665d8765b13662071c93
fabrice <fabrice@fabrice-PC>
2016-03-03 22:02:52 +0100

    Modification security.yml :: add login/logout data

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/homepage", name="homepage")
      */
     public function indexAction()
     {
